f42f9a77dea31945cb634c50f4000d5461981ba6
shri1906 <rajm46069@gmail.com>
2025-02-13 12:39:26 +0530

    salry list created

--- client/src/components/salary/ViewSalary.jsx
+++ client/src/components/salary/ViewSalary.jsx
@@ -26,11 +26,75 @@ const ViewSalary = () => {
     }
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchSalaries();
-  },[])
-  
-  return <div>ViewSalary</div>;
+  }, []);
+
+  const filterSalaries = (q) => {
+    const filteredRecords = salaries.filter((leave) => {
+      leave.employeeId.toLocaleLowerCase().includes(q.toLocaleLowerCase());
+    });
+    setFilteredSalaries(filteredRecords);
+  };
+
+  return (
+    <>
+      {filteredSalaries === null ? (
+        <div>Loading...</div>
+      ) : (
+        <div className="overflow-x-auto p-5">
+          <div className="text-center">
+            <h2 className="text-2xl font-bold">Salary History</h2>
+          </div>
+          <div className="flex justify-end my-3">
+            <input
+              type="text"
+              placeholder="Search by Emp ID"
+              className="border px-2 rounded-md py-0.5 border-gray-300"
+              onChange={filterSalaries}
+            />
+          </div>
+          {filteredSalaries.lenght > 0 ? (
+            <table className="w-full text-sm text-left text-gray-500">
+              <thead className="text-xs text-gray-700 uppercase bg-gray-50 border border-gray-200">
+                <tr>
+                  <th className="px-6 py-3">SNo</th>
+                  <th className="px-6 py-3">Emp ID</th>
+                  <th className="px-6 py-3">Salary</th>
+                  <th className="px-6 py-3">Allowance</th>
+                  <th className="px-6 py-3">Deducation</th>
+                  <th className="px-6 py-3">Total</th>
+                  <th className="px-6 py-3">Pay Date</th>
+                </tr>
+              </thead>
+              <tbody>
+                {filteredSalaries.map((salary) => (
+                  <tr
+                    key={salary.id}
+                    className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                  >
+                    <td className="px-6 py-3">{sno++}</td>
+                    <td className="px-6 py-3">
+                      {salary.employeeId.employeeId}
+                    </td>
+                    <td className="px-6 py-3">{salary.basicSalary}</td>
+                    <td className="px-6 py-3">{salary.allowance}</td>
+                    <td className="px-6 py-3">{salary.deductions}</td>
+                    <td className="px-6 py-3">{salary.netSalary}</td>
+                    <td className="px-6 py-3">
+                      {new Date(salary.payDate).toLocaleDateString()}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <div>No records found</div>
+          )}
+        </div>
+      )}
+    </>
+  );
 };
 
 export default ViewSalary;
