109713bf587901566d378b302f3f627c65445a56
shri1906 <rajm46069@gmail.com>
2025-03-05 17:13:11 +0530

    cors updated

--- server/index.js
+++ server/index.js
@@ -13,12 +13,13 @@ import attendanceRouter from "./routes/attendance.js";
 connectToDatabase();
 const app = express();
 
-app.use(cors(
-  {
-    origin: 'https://ems-frontend-sand.vercel.app',
-    credentials: true
-  }
-));
+app.use(cors({
+  origin: ['https://ems-frontend-sand.vercel.app'], 
+  methods: "GET,POST,PUT,DELETE", 
+  allowedHeaders: "Content-Type,Authorization", 
+  credentials: true, 
+}));
+
 app.use(express.json());
 
 app.use("/uploads", express.static("public/uploads"));
